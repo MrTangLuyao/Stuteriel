c6dbd4c0821dca0d6754d1d761ca2cb1e9f0ba9e
NickTTTTTTTTTang <320680273@qq.com>
2022-08-11 15:57:15 +0800

    Update0811
    增加书籍管理程序演示代码文件

--- SourceCode/contentlist/contentlist/struct.hpp
+++ SourceCode/contentlist/contentlist/struct.hpp
@@ -1,15 +1,1 @@
-#include <string>
-#define MAX 1000
-struct book
-{
-    string name;
-    string owner;
-    string ISSN;
-    string otherDetails;
-};
 
-struct bookList
-{
-    book bookArray[MAX];
-    int size;
-};
